01715e7286b1d631f072e7e7c389e18f8ec4e919
Antoine Riard <ariard@student.42.fr>
2017-05-25 20:11:33 +0200

    branch condition test

---
+++ debug/driver.c
@@ -0,0 +1,7 @@
+extern int	Hello();
+
+int		main(void)
+{
+	Hello();
+	return (0);
+}
---
+++ debug/testdebug.c
@@ -0,0 +1,11 @@
+extern int	print_string(char *str);
+
+extern int 	read_char(void);
+
+extern int	print_char(char c);
+
+int		main(void)
+{
+	print_char('A');
+	return (0);
+}
